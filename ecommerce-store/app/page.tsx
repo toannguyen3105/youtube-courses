2b1b367fcf5b1aba4c60fb91bec3adf20b126a95
Tony Nguyen <nguyenhuytoan1994@outlook.com>
2023-11-03 19:40:35 +0700

    01. Environment setup

---
+++ ecommerce-store/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      Started page
+      <hr />
+      <Button variant="outline">New Button</Button>
+    </>
+  );
+};
+
+export default Home;
